f693f943001354ebdc8075997f8bf6dad3cb3d57
Aivuo <rioneda1@gmail.com>
2018-06-08 15:57:12 +0200

    So much stuff

---
+++ Models/StudentsViewModel.cs
@@ -0,0 +1,30 @@
+﻿using SchoolSystemClasses;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SchoolSystem.Models
+{
+    public class StudentsViewModel
+    {
+        public int StudentId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public List<Course> Courses { get; set; }
+
+        public StudentsViewModel(Student student)
+        {
+            StudentId = student.StudentId;
+            FirstName = student.FirstName;
+            LastName = student.LastName;
+
+            Courses = new List<Course>();
+
+            foreach (var item in student.Courses)
+            {
+                Courses.Add(item);
+            }
+        }
+    }
+}
---
+++ Models/TeachersViewModel.cs
@@ -0,0 +1,33 @@
+﻿using SchoolSystemClasses;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SchoolSystem.Models
+{
+    public class TeachersViewModel
+    {
+        public int TeacherId { get; set; }
+        public string FirstName { get; set; }
+        public string MiddleName { get; set; }
+        public string LastName { get; set; }
+        public List<Course> Courses { get; set; }
+
+        public TeachersViewModel(Teacher teacher)
+        {
+            TeacherId = teacher.TeacherId;
+            FirstName = teacher.FirstName;
+            MiddleName = teacher.MiddleName;
+            LastName = teacher.LastName;
+
+            Courses = new List<Course>();
+
+            foreach (var item in teacher.Courses)
+            {
+                Courses.Add(item);
+            }
+
+        }
+    }
+}
